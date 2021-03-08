2b6cbfa1f51fb344966a883761eb6a442e1cf066
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-08 09:44:47 -0500

    poggers

--- server/test.ts
+++ server/test.ts
@@ -7,7 +7,8 @@ let frame = {
     width: 5,
     height: 5,
     players: {
-        a: {x: 0, y: 0}
+        a: {x: 0, y: 0},
+        b: {x: 4, y: 4}
     },
     blocks: [
         {x: 2, y: 2, w: 1, h: 1}
@@ -20,7 +21,11 @@ const inputs =
         [{player: "a", action: "down"}],
         [{player: "a", action: "down"}],
         [{player: "a", action: "right"}],
+        [{player: "b", action: "up"}],
+        [{player: "b", action: "up"}],
+        [{player: "b", action: "left"}],
     ]
+
 
 inputs.forEach(input => frame = step(frame, input))
 
