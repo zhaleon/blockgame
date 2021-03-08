d2324c6e9b0766013c59f501b0f7895bf4f9a9c2
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-08 14:51:10 -0500

    changes

--- server/sandbox.ts
+++ server/sandbox.ts
@@ -1,10 +1,14 @@
-import {addPlayer, createFrame, frame, input} from "./tests/helper";
+import {addBlock, addPlayer, createFrame, frame, input} from "./tests/helper";
 
 createFrame(5, 5)
 addPlayer("a", 0, 0)
 addPlayer("b", 4, 4)
+addBlock(2, 2, 1, 1)
+
 input("a_down", "b_right")
 for (let i = 0; i <= 3; i++) input("a_down")
 for (let i = 0; i <= 4; i++) input("a_right")
-console.log(frame.players.a.x == 3)
-console.log(frame.blocks[0].x == 4)
+console.log(frame.players.b.x == 3)
+console.log(frame.players.b.y == 4)
+console.log(frame.players.b.x == 4)
+console.log(frame.players.b.y == 4)
