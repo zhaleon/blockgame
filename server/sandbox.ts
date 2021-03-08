44210e932e3a76404df0a5549c96a3327fea9de4
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-08 14:49:32 -0500

    changes

--- server/sandbox.ts
+++ server/sandbox.ts
@@ -1,32 +1,10 @@
-//test.ts
-import {step} from "./main"
+import {addPlayer, createFrame, frame, input} from "./tests/helper";
 
-import {strict} from "assert";
-
-let frame = {
-    width: 5,
-    height: 5,
-    players: {
-        a: {x: 0, y: 0},
-    },
-    blocks: [
-        {x: 2, y: 2, w: 1, h: 1}
-    ]
-};
-
-
-const inputs =
-    [
-        [{player: "a", action: "down"}],
-        [{player: "a", action: "down"}],
-        [{player: "a", action: "right"}],
-        [{player: "a", action: "right"}],
-        [{player: "a", action: "right"}],
-        [{player: "a", action: "right"}],
-        [{player: "a", action: "right"}],
-    ]
-
-inputs.forEach(input => frame = step(frame, input))
-
+createFrame(5, 5)
+addPlayer("a", 0, 0)
+addPlayer("b", 4, 4)
+input("a_down", "b_right")
+for (let i = 0; i <= 3; i++) input("a_down")
+for (let i = 0; i <= 4; i++) input("a_right")
 console.log(frame.players.a.x == 3)
 console.log(frame.blocks[0].x == 4)
