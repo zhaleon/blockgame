c27afa7741e6d64cc0202f7c74d614f6030b6eef
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-09 13:08:38 -0500

    changes

--- client/src/border.tsx
+++ client/src/border.tsx
@@ -1,15 +1,50 @@
-import {Component} from "react";
+import React, {Component, createRef} from "react";
 import {list} from "./utils";
-import React = require("react");
+import anime from "animejs";
+import {compRef} from "./index";
+import {addBlock, addPlayer, createFrame, frame, input} from "../../server/tests/helper";
+
+let playerALast;
+let playerBLast;
+
+function reset() {
+    playerALast = playerBLast = null;
+    createFrame(5, 5)
+    addPlayer("a", 0, 0)
+    addPlayer("b", 4, 4)
+    addBlock(0, 3, 2, 1, 1)
+
+}
+
+reset()
+
 
 export class Border extends Component<{ width: number, height: number, }> {
+    ref: any = createRef()
+
+    componentDidMount() {
+        document.onkeydown = e => {
+            if (e.key == 'w') playerALast = 'a_up';
+            else if (e.key == 's') playerALast = 'a_down';
+            else if (e.key == 'a') playerALast = 'a_left';
+            else if (e.key == 'd') playerALast = 'a_right';
+            else if (e.key == 'r') reset()
+            else return
+
+        };
+
+    }
+
     render() {
         const {width, height} = this.props
+        const offset = 0.2;
         return (
             <>
                 <rect x={-1} y={-1} width={width + 2} height={height + 2} fill={"#eee"}/>
+                <rect id={"timer"} x={-1} y={-1} width={width + 2} height={offset} fill={"#e00"}/>
+                <rect x={-1} y={-1 + offset} width={width + 2} height={1 - offset} fill={"#333"}/>
+
                 <rect x={-1} y={-1} width={1} height={height + 2} fill={"#333"}/>
-                <rect x={-1} y={-1} width={width + 2} height={1} fill={"#333"}/>
                 <rect x={-1} y={height} width={width + 2} height={1} fill={"#333"}/>
 
                 <rect x={width} y={-1} width={1} height={height + 2} fill={"#333"}/>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,45 +1,24 @@
 import "./style.css"
 import {Board} from "./board";
-import {Component} from "react";
-import {addBlock, addPlayer, createFrame, frame, input} from "../../server/tests/helper";
-
-var React = require('react');
-var ReactDOM = require('react-dom');
+import {Component, createRef, Ref} from "react";
+import {addBlock, addPlayer, createFrame, frame} from "../../server/tests/helper";
+const React = require('react');
+const ReactDOM = require('react-dom');
 let node = document.createElement('div');
 document.body.appendChild(node)
-let playerALast;
-let playerBLast;
-
-function reset() {
-    playerALast = playerBLast = null;
-    createFrame(5, 5)
-    addPlayer("a", 0, 0)
-    addPlayer("b", 4, 4)
-    addBlock(0, 3, 2, 1, 1)
-
-}
-
-reset()
 
 
 class MyComponent extends Component {
     state = frame
 
     componentDidMount() {
-        setInterval(() => {
-            input(...[playerALast, playerBLast].filter(item => item))
-            playerALast = playerBLast = null;
-            this.setState(frame)
-        }, 250)
-        document.onkeydown = e => {
-            if (e.key == 'w') playerALast = 'a_up';
-            else if (e.key == 's') playerALast = 'a_down';
-            else if (e.key == 'a') playerALast = 'a_left';
-            else if (e.key == 'd') playerALast = 'a_right';
-            else if (e.key == 'r') reset()
-            else return
+        // setInterval(() => {
+        //     input(...[playerALast, playerBLast].filter(item => item))
+        //     playerALast = playerBLast = null;
+        //     this.setState(frame)
+        // }, 250)
 
-        };
+
     }
 
     render() {
@@ -48,5 +27,6 @@ class MyComponent extends Component {
 
 }
 
+export const compRef: Ref<MyComponent> = createRef()
 
-ReactDOM.render(<MyComponent/>, node);
+ReactDOM.render(<MyComponent ref={compRef}/>, node);
