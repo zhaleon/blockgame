6f888f57bf4df407e23a1c0945bcdd275437bbb0
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-09 10:19:58 -0500

    something deeply bork

--- server/sandbox.ts
+++ server/sandbox.ts
@@ -2,7 +2,7 @@ import {addBlock, addPlayer, createFrame, frame, input} from "./tests/helper";
 
 createFrame(5, 5)
 addPlayer("a", 0, 0)
-addBlock(0, 3, 2, 1, 1)
+addBlock(0, 2, 2, 1, 1)
 input("a_down")
 input("a_down")
 input("a_right")
