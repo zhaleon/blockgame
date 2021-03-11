03ee28eb760c4d1f0abb8225c6a014a69e55e982
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-11 16:59:59 -0500

    changes

--- client/src/block.tsx
+++ client/src/block.tsx
@@ -7,7 +7,7 @@ export class Block extends Comp {
     render() {
         // const {x, y, width, height} = this.props;
         return <Spring to={this.props}>
-            {spring => <animated.rect {...spring} strokeWidth={0.025} fill="#0074D9" stroke={"rgba(0,0,0,0.4)"}/>}
+            {spring => <animated.rect {...spring} strokeWidth={0.025} fill={"#0074D9"} stroke={"rgba(0,0,0,0.4)"}/>}
         </Spring>
     }
 }
