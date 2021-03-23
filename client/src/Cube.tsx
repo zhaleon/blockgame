107bd9e1c7a51cd05c61080fdab8790dbb38ee1b
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-23 09:31:55 -0400

    3d animation

--- client/src/Cube.tsx
+++ client/src/Cube.tsx
@@ -1,15 +1,20 @@
 import React, {useRef} from "react";
+import {animated, useSpring} from 'react-spring/three'
 
 export function Cube({x, y, width = 1, height = 1, color, strokeColor}) {
     const geom: any = useRef()
-    return (
-        <mesh
-            position={[x + width / 2, 0, y + height / 2]}>
-            <boxBufferGeometry ref={geom} args={[width, 1, height]}/>
-            <meshStandardMaterial color={color}/>
-            <Edges geometry={geom} strokeColor={strokeColor}/>
-        </mesh>
-    )
+    const props = useSpring({
+        'position-x': x + width / 2,
+        'position-z': y + height / 2,
+        from: {'position-x': 0, 'position-z': 0}
+    })
+    return <animated.mesh
+        {...props}
+        scale={[width, 1, height]}>
+        <animated.boxBufferGeometry ref={geom}/>
+        <animated.meshStandardMaterial color={color}/>
+        <Edges geometry={geom} strokeColor={strokeColor}/>
+    </animated.mesh>
 }
 
 function Edges({geometry, strokeColor}) {
