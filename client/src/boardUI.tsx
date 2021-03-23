0b42311b595936c2306a9ed2ed962e196a6da14b
Karthikeya Kona <konakarthik12@gmail.com>
2021-03-22 23:12:59 -0400

    player is yellow

--- client/src/boardUI.tsx
+++ client/src/boardUI.tsx
@@ -14,14 +14,14 @@ export function BoardUI({frame}: { frame: { blocks: Map<string, Entity>, width, 
     //     {map(blocks, ({id, ...props}) => <Block key={id} {...props}/>)}
     //     {map(players, ({id, ...props}) => <Player key={id} {...props}/>)}
     // </svg>;
-    return <Canvas camera={{position: [5, 10, 5], zoom: 60}} orthographic
+    return <Canvas camera={{position: [5, 10, 5], zoom: 70}} orthographic
                    onCreated={({camera}) => camera.lookAt(5, 0, 5)}>
         <ambientLight/>
         <pointLight position={[10, 10, 10]}/>
+        {map(players, ({id, ...props}) => <Cube color={"#ff0"} strokeColor={'#b77c00'} key={id} {...props}/>)}
         {map(blocks, ({id, ...props}) => <Cube color={'hotpink'} strokeColor={'red'} key={id} {...props}/>)}
-        {map(players, ({id, ...props}) => <Cube color={'green'} strokeColor={'blue'} key={id} {...props}/>)}
 
-        <gridHelper args={[width, height]} position={[width / 2, -0.5, height / 2]}/>
+        <gridHelper args={[width, height, 0x888888]} position={[width / 2, -0.5, height / 2]}/>
         <Stats/>
 
     </Canvas>
