f43fdb6c289335bec12c73a81282913b9c37d7a4
leon <lionz2020@gmail.com>
2021-04-06 13:32:17 -0400

    changes

--- server/physics.ts
+++ server/physics.ts
@@ -15,19 +15,41 @@ enum Direction {
 function adjacent(a: Entity, b: Entity, dir: number) : boolean {
     switch (dir) {
         case 0: {
-            if (b.y + b.height == a.y && b.width == 1 && b.x == a.x) return true   
+            if (a.y + a.height == b.y && b.x <= a.x && a.x < b.x + b.width) return true;
             break
         }
         case 1: {
-            if (b.y == a.y + a.height && b.width == 1 && b.x == a.x) return true   
+            if (b.y + b.height == a.y && b.x <= a.x && a.x < b.x + b.width) return true;
             break
         }
         case 2: {
-            if (b.x + b.width == a.x && b.height == 1 && b.y == a.y) return true   
+            if (a.x + a.width == b.x && b.y <= a.y && a.y < b.y + b.height) return true; 
             break
         }
         case 3: {
-            if (b.x == a.x + a.width && b.width == 1 && b.y == a.y) return true   
+            if (a.x == b.x + b.width && b.y <= a.y && a.y < b.y + b.height) return true;
+            break
+        }
+    } 
+    return false;
+}
+
+function willMove(a: Entity, b: Entity, dir: number) : boolean {
+    switch (dir) {
+        case 0: {
+            if (a.y + a.height == b.y && b.width == 1 && b.x == a.x) return true   
+            break
+        }
+        case 1: {
+            if (a.y == b.y + b.height && b.width == 1 && b.x == a.x) return true   
+            break
+        }
+        case 2: {
+            if (a.x + a.width == b.x && b.height == 1 && b.y == a.y) return true   
+            break
+        }
+        case 3: {
+            if (a.x == b.x + b.width && b.height == 1 && b.y == a.y) return true   
             break
         }
     } 
@@ -50,22 +72,32 @@ export function updateBoard(board: Board, player: Player, dir: string) {
     let toMove = [] 
     let canMove = true
     let lastBlock = null 
+    let reps = 0
     while (true) {
+        if (++reps > 100) break
         let ok = false
         for (const [_,block] of board.blocks) {
             if (adjacent(lastBlock ?? player, block, Direction[dir])) {
-                ok = true
-                lastBlock = block
-                toMove.push(block.id)
+                if (willMove(lastBlock ?? player, block, Direction[dir])) {
+                    ok = true
+                    lastBlock = block
+                    toMove.push(block.id)
+                } else {
+                    console.log(lastBlock ?? player, block)
+                    canMove = false;
+                }
                 break
             }  
         }
-        if (!ok) break
+        if (!ok || !canMove) break
     }
-    board.players[player.id].x += dx[Direction[dir]]
-    board.players[player.id].y += dy[Direction[dir]]
-    toMove.forEach(i => {
-        board.blocks[i].x += dx[Direction[dir]] 
-        board.blocks[i].y += dy[Direction[dir]]
-    })
+    // console.log(player)
+    if (canMove) {
+        board.players.get(player.id).x += dx[Direction[dir]]
+        board.players.get(player.id).y += dy[Direction[dir]]
+        toMove.forEach(i => {
+            board.blocks.get(i).x += dx[Direction[dir]] 
+            board.blocks.get(i).y += dy[Direction[dir]]
+        })
+    }
 }
