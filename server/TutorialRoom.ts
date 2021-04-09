238d0c183904baff6cd74b25e57bb2815841cf17
leon <lionz2020@gmail.com>
2021-04-09 15:26:58 -0400

    changes

--- server/TutorialRoom.ts
+++ server/TutorialRoom.ts
@@ -1,11 +1,13 @@
 import { Room, Client } from "colyseus"
 import Player from "./player"
 import Board from "./board"
+import * as level from "../levels/1.json"
 
 export class TutorialRoom extends Room {
     onCreate(options: any) {
         this.maxClients = 1 
         this.setState(new Board(4,4))
+        this.state.loadFromJSON(level)
     }
 
     onJoin(client: Client, options: any) {
