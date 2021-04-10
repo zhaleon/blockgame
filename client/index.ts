fc8fefa7d12663435cc6cec35c0ee76a7f4cc673
Karthikeya Kona <konakarthik12@gmail.com>
2021-04-09 22:58:23 -0400

    test heroku integration

--- client/index.ts
+++ client/index.ts
@@ -3,9 +3,9 @@ import config from "../webpack.config";
 import express from "express";
 
 export function attachFrontend(app): Promise<void> {
-    let configuration = config(null, {mode: "production"});
+    let configuration = config(null, {mode: process.env.dev ? "development" : "production"});
     return new Promise((res, rej) => {
-        let data = webpack(configuration, (err, stats) => {
+        let data = webpack(configuration, err => {
             if (err) rej(err)
             app.use(express.static(data.outputPath))
             res()
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const port = +process.env.port || 8080;
 async function main() {
     const app = express()
     await attachFrontend(app);
-    const server = app.listen(8080)
+    const server = app.listen(port)
     await attachBackend(server)
     console.log("Listening on port " + port)
 }
